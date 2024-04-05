b50f2aa9078975036ab14a9d27e3dbae7995157c
Caio Zamana <caiobzm@gmail.com>
2024-04-05 14:15:35 -0300

    Atualização criptografando password funcionary

--- src/main/java/Santander/Loan/service/implementation/FuncionaryServiceImpl.java
+++ src/main/java/Santander/Loan/service/implementation/FuncionaryServiceImpl.java
@@ -7,20 +7,24 @@ import Santander.Loan.reposiroty.UserRepository;
 
 import Santander.Loan.service.interfaces.IFuncionaryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class FuncionaryServiceImpl implements IFuncionaryService {
 
     private final FuncionaryRepository funcionaryRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Autowired
-    public FuncionaryServiceImpl(FuncionaryRepository funcionaryRepository, UserRepository userRepository) {
+    public FuncionaryServiceImpl(FuncionaryRepository funcionaryRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.funcionaryRepository = funcionaryRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createFuncionary(Funcionary funcionary) {
@@ -30,13 +34,24 @@ public class FuncionaryServiceImpl implements IFuncionaryService {
         if (funcionaryRepository.existsByEmail(funcionary.getEmail())) {
             throw new BusinessException("E-mail já está sendo utilizado.");
         }
-        if (funcionaryRepository.existsByUsername(funcionary.getUsername())) {
+        if (userRepository.existsByUsername(funcionary.getUsername())) {
             throw new BusinessException("Username já está sendo utilizado.");
         }
+        if (!isNumericPassword(funcionary.getPassword())) {
+            throw new BusinessException("A senha deve conter entre 6 e 10 dígitos numéricos.");
+        }
+
+        // Criptografa a senha antes de salvar
+        String encryptedPassword = passwordEncoder.encode(funcionary.getPassword());
+        funcionary.setPassword(encryptedPassword);
 
         funcionaryRepository.save(funcionary);
     }
 
+    private boolean isNumericPassword(String password) {
+        // Utiliza uma expressão regular para verificar se a senha contém apenas dígitos numéricos
+        return Pattern.matches("^\\d{6,10}$", password);
+    }
 
     public void updateFuncionary(Funcionary updatedFuncionary){
         try {
